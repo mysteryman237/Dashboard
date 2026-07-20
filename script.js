af5c5b00c0b9a62b70bef094f937ff2f33804f35
mysteryman237 <claudeadolf46@gmail.com>
2026-07-20 18:00:16 +0400

    initail commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+const dashBMenuIcon = document.getElementById('dashboard-brand');
+const sideBar = document.getElementById('side-bar')
+const sideBCloseIcon = document.getElementById('side-icon')
+const overlay = document.querySelector('.overlay')
+const mainDasboard = document.querySelector('main')
+const navBar = document.querySelector('nav')
+
+dashBMenuIcon.addEventListener('click', () => {
+   isClosed()
+})
+sideBCloseIcon.addEventListener('click', () => {
+    isOpen()
+})
+overlay.addEventListener('click', () => {
+   isOpen()
+})
+// This function opens the sidebar and displays the overlay
+function isClosed(){
+ sideBar.classList.add('showAside')
+ overlay.classList.add('showOverlay')
+ mainDasboard.classList.add('hide')
+ navBar.classList.add('hide')
+}
+// This function closes the sidebar and hides the overlay
+function isOpen(){
+ sideBar.classList.remove('showAside')
+ overlay.classList.remove('showOverlay')
+ mainDasboard.classList.remove('hide')
+ navBar.classList.remove('hide')
+}
+//add event listener to the activity button to show full activity
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', event => {
+   const container = document.querySelector('.activity-group')
+   container.classList.toggle('full-activity')
+   container.classList.contains('full-activity')? btn.textContent = "Show Less" : btn.textContent = "View All"
+      
+})
+const statusCells = document.querySelectorAll('.status')
+statusCells.forEach(status => {
+   if(status.textContent === "Pending") status.style.color = 'gold'
+   if(status.textContent === "Delivered") status.style.color = 'green'
+   if(status.textContent === "Cancelled") status.style.color = 'red'
+})
